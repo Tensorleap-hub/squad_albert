8a89fb927e886d2cd13cfe99c36705d10272452f
Chen Rothschild <135505576+Chen-Roth@users.noreply.github.com>
2025-04-23 10:24:44 +0300

    Fix loss is (#6)
    * edit vis to have batch size+numpy inputs
    
    * edit custom test
    
    * edit loss to have numpy with batch size inputs
    
    * edit metrics to have numpy with batch size inputs
    
    * add packages of code-loaders-helpers and lxml-html-clean

--- squad_albert/loss.py
+++ squad_albert/loss.py
@@ -1,20 +1,22 @@
 import tensorflow as tf
-
+import numpy as np
 from squad_albert.metrics import get_start_end_arrays
 
 
-def CE_loss(ground_truth: tf.Tensor, prediction: tf.Tensor) -> tf.Tensor:
+def CE_loss(ground_truth: np.ndarray, prediction: np.ndarray) -> np.ndarray:
     """
     Description: Computes the combined Categorical Cross-Entropy loss for start and end index predictions.
     Parameters:
-    ground_truth (tf.Tensor): Ground truth tensor of shape [B, max_sequence_length, 2].
-    prediction (tf.Tensor): Predicted tensor of shape [B, max_sequence_length, 2].
+    ground_truth (np.ndarray): Ground truth tensor of shape [B, max_sequence_length, 2].
+    prediction (np.ndarray): Predicted tensor of shape [B, max_sequence_length, 2].
     Returns:
-    combined_loss (tf.Tensor): Combined loss for start and end index predictions, computed as the sum of individual Categorical Cross-Entropy losses weighted by alpha.
+    combined_loss (np.ndarray): Combined loss for start and end index predictions, computed as the sum of individual Categorical Cross-Entropy losses weighted by alpha.
     """
-    loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    ground_truth = tf.convert_to_tensor(ground_truth)
+    prediction = tf.convert_to_tensor(prediction)
+    loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True, reduction='none')
     alpha = 1.0
     start_pred, end_pred = get_start_end_arrays(prediction)
     start_gt, end_gt = get_start_end_arrays(ground_truth)
     combined_loss = loss(start_gt, start_pred) + alpha * loss(end_gt, end_pred)
-    return combined_loss
+    return combined_loss.numpy()
